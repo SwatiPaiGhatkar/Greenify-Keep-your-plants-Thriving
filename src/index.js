39afba542fe51e08bcd32e55f3964cd8c7c819f3
SwatiPaiGhatkar <swatipaighatkar@gmail.com>
2025-06-11 13:33:59 +1000

    added Basic form handling in JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-alert("hello");
+function generateInformation(event) {
+  event.preventDefault();
+
+  let careGuideElement = document.querySelector("#care-guide");
+  careGuideElement.innerHTML = " Your plant guide here..";
+}
+
+let plantCareElement = document.querySelector("#plant-care-form");
+plantCareElement.addEventListener("submit", generateInformation);
